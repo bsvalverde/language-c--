3fdbe09b467a49adecc115f84807bea47c84b6fa
maikeps <psmaike@gmail.com>
2016-06-21 17:14:24 -0300

    Corrigido erro de chamada de método.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
     //yydebug = 1;
     yyparse();                  //parses whole data
     std::cout << std::endl;
-    std::cout << root->printTree();   //prints the ASTs
+    std::cout << root->analyzeTree();   //prints the ASTs
     std::cout << std::endl;
     // root->computeTree(); //computes the ASTs
     return 0;
